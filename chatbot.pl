fb8aea749ca35a6980f6d96e76a2fff58bbbe49f
Russ Henson <henson.rj10@gmail.com>
2019-11-19 17:05:11 +0800

    emergency or not

--- chatbot.pl
+++ chatbot.pl
@@ -1,7 +1,20 @@
+yes_ans(yes).
+no_ans(no).
+
 start :-
-    write('Hello! I am Dr.Bot, your medical chatbot.'),
+    write('Hello! I am Dr.Bot, your medical chatbot assistant.'),
     nl,
     write('What is your name? '),
     read(Name), nl,
-    format('Hi, ~w!~n', [Name]).
+    format('Hi, ~w!~n', [Name]),
+    write('Is this an emergency? (yes or no)'),
+    read(Answer), nl, no_ans(Y),
+    Answer=@=Y,nl,not_emergency;
+    emergency.
 
+
+not_emergency :-
+    write('Please refer to a large medical facility.').
+
+emergency :-
+    write('What is your symptom?').
