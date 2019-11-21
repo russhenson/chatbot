02bf5ae8fb89570fd34f04474dfbd54646f9664f
Russ Henson <henson.rj10@gmail.com>
2019-11-21 20:07:07 +0800

    hakdog

--- chatbot.pl
+++ chatbot.pl
@@ -13,44 +13,71 @@ start :-
     write('Is this an emergency? (yes or no)'), nl,
     read(Answer), nl,
     Answer=@='no',nl,not_emergency;
-    emergency.
+    queries.
 
 
 
 not_emergency :-
     write('Please refer to a large medical facility.').
-
-emergency :-
-    write('Do you have a cough?'),
+queries :-
+    write('Do you have a cough?'), nl,
     read(Answer), nl,
     Answer=@='yes', nl, cough_symptom;
     write('Do you have high fever?'), nl,
     read(Answer1), nl,
     Answer1=@='yes', nl, high_fever_symptom;
+    write('Do you have low fever?'), nl,
+    read(Answer1), nl,
+    Answer1=@='yes', nl, fever_symptom;
     idk.
 
+%coughing symptom: tuberculosis, pneumonia, influenza, asthma
 cough_symptom :-
     patient(Name),
     assert(coughing(Name)),
-    write('Do you smoke?').
+    write('Have you experienced coughing with blood?'), nl,
+    read(Answer), nl,
+    Answer=@='yes', assert(coughing_with_blood(Name)); %Tuberculosis
+    write('Do you have allergies?'), nl,
+    read(Answer1), nl,
+    Answer1=@='yes', assert(allergies(Name)); %asthma
+    write('Do you have nasal congestion?'), nl,
+    read(Answer2), nl,
+    Answer2=@='yes', assert(nasal_congestion(Name)); %influenza
+    idk. %conclude pneumonia
 
+%high fever: malaria, dengue fever, leptos, influenza
 high_fever_symptom :-
     patient(Name),
     assert(high_fever(Name)),
-    write('high fever').
+    write('Have you had recent contact with flood, water, or soil?'), nl,
+    read(Answer), nl,
+    Answer=@='yes', assert(contaminated_water_contact(Name)); %leptos
+    write('Do you have nasal congestion?'), nl,
+    read(Answer1), nl,
+    Answer1=@='yes', assert(nasal_congestion(Name)); %influenza
+    write('Do you have mosquito bite?'), nl,
+    read(Answer2), nl,
+    Answer2=@='yes', nl,
+    write('Do you have skin rash?'), nl,
+    read(Answer3), nl,
+    Answer3=@='yes', assert(dengue_fever(Name)); %dengue
+    idk. %conclude malaria
+
+%fever: pneumonia, measles, tb, rabies, athlete's foot, hepa B
+fever_symptom :-
+    patient(Name),
+    assert(fever(Name)),
+
+
+
+
 
 idk :-
     write('hatdog').
 
 
-lagnat(X) :- high_fever(X).
 
 
 
-diagnosis(Symptom, Cause):- cause(X, Cause), symptom(X, Symptom),
-    write('Your possible sicknesses are: '), nl,
-    write(X).
-
-
-retract :-
 
