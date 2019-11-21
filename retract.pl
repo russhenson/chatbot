0fbf5c9cb1d8926715baf5e141f085b5eeec8c10
GG <gerardstjosol@users.noreply.github.com>
2019-11-21 17:42:59 +0800

    hatdog

--- retract.pl
+++ retract.pl
@@ -7,85 +7,96 @@ retract :-
     retract(coughing(X)),
     retract(nausea(X)),
     retract(vomiting(X)),
-swollen_lymph_glands(X).
-muscle_pain(X).
-joint_pain(X).
-skin_rash(X).
-bleeding_nose_and_gums(X).
-bruising_skin(X).
-red_bumps(X).
-blisters(X).
-scabs(X).
-itchy_skin(X).
-open_wound(X).
-contaminated_soil_contact(X). %for leptospirosis: water OR soil
-chills(X).
-yellow_skin(X).
-abdominal_pain(X).
-diarrhea(X).
-allergies(X).
-smoking(X).
-exercised(X).
-heartburn(X).
-sinusitis(X).
-took_medication(X).
-exposed_to_smoke(X).
-shortness_of_breath(X).
-chest_pain(X).
-coughing(X).
-wheezing(X).
-sore_throat(X).
-nasal_congestion(X).
-runny_nose(X).
-fatigue(X).
-confusion(X).
-lower_body_temperature(X).
-exposed_to_measles(X).
-dry_cough(X).
-inflamed_eyes(X).
-kopliks_spots(X).
-red_eyes(X).
-stinging_eyes(X).
-itchy_eyes(X).
-blurry_vision(X).
-tearing(X).
-photophobia(X).
-discharge_from_eye(X).
-foreign_body_sensation(X).
-exposed_to_tb(X).
-weight_loss(X).
-low_appetite(X). %same as loss_of_appetite
-coughing_with_blood(X).
-night_sweats(X).
-fever(X).
-bitten_by_rabid_animal(X).
-agitation(X).
-anxiety(X).
-hyperactivity(X).
-difficulty_swallowing(X).
-excessive_salivation(X).
-fear_from_drinking_fluids(X).
-hallucinations(X).
-insomnia(X).
-partial_paralysis(X).
-rapid_dehydration(X).
-dry_tongue(X).
-thirsty(X).
-blood_pressure_falls(X).
-faint_pulse(X).
-muscular_cramps(X).
-hollow_sunken_eyes(X).
-wrinkled_skin(X).
-lethargy(X).
-seizures(X).
-red_rash_between_toes(X).
-itchy_feet(X).
-dryness(X).
-scaling(X).
-contaminated_food_contact(X).
-contaminated_water_contact(X).
-stomach_pain(X).
-hepa_b_infected_contact(X).
-light_colored_poop(X).
-belly_pain(X).
-mosquito_bite(X).
+    retract(swollen_lymph_glands(X)),
+    retract(muscle_pain(X)),
+    retract(joint_pain(X)),
+    retract(skin_rash(X)),
+    retract(bleeding_nose_and_gums(X)),
+    retract(bruising_skin(X)),
+    retract(red_bumps(X)),
+    retract(blisters(X)),
+    retract(scabs(X)),
+    retract(itchy_skin(X)),
+    retract(open_wound(X)),
+    retract(contaminated_soil_contact(X)), %for leptospirosis: water OR soil
+    retract(chills(X)),
+    retract(yellow_skin(X)),
+    retract(abdominal_pain(X)),
+    retract(diarrhea(X)),
+    retract(allergies(X)),
+    retract(smoking(X)),
+    retract(exercised(X)),
+    retract(heartburn(X)),
+    retract(sinusitis(X)),
+    retract(took_medication(X)),
+    retract(exposed_to_smoke(X)),
+    retract(shortness_of_breath(X)),
+    retract(chest_pain(X)),
+    retract(coughing(X)),
+    retract(wheezing(X)),
+    retract(sore_throat(X)),
+    retract(nasal_congestion(X)),
+    retract(runny_nose(X)),
+    retract(fatigue(X)),
+    retract(confusion(X)),
+    retract(lower_body_temperature(X)),
+    retract(exposed_to_measles(X)),
+    retract(dry_cough(X)),
+    retract(inflamed_eyes(X)),
+    retract(kopliks_spots(X)),
+    retract(red_eyes(X)),
+    retract(stinging_eyes(X)),
+    retract(itchy_eyes(X)),
+    retract(blurry_vision(X)),
+    retract(tearing(X)),
+    retract(photophobia(X)),
+    retract(discharge_from_eye(X)),
+    retract(foreign_body_sensation(X)),
+    retract(exposed_to_tb(X)),
+    retract(weight_loss(X)),
+    retract(low_appetite(X)), %same as loss_of_appetite
+    retract(coughing_with_blood(X)),
+    retract(night_sweats(X)),
+    retract(fever(X)),
+    retract(bitten_by_rabid_animal(X)),
+    retract(agitation(X)),
+    retract(anxiety(X)),
+    retract(hyperactivity(X)),
+    retract(difficulty_swallowing(X)),
+    retract(excessive_salivation(X)),
+    retract(fear_from_drinking_fluids(X)),
+    retract(hallucinations(X)),
+    retract(insomnia(X)),
+    retract(partial_paralysis(X)),
+    retract(rapid_dehydration(X)),
+    retract(dry_tongue(X)),
+    retract(thirsty(X)),
+    retract(blood_pressure_falls(X)),
+    retract(faint_pulse(X)),
+    retract(muscular_cramps(X)),
+    retract(hollow_sunken_eyes(X)),
+    retract(wrinkled_skin(X)),
+    retract(lethargy(X)),
+    retract(seizures(X)),
+    retract(red_rash_between_toes(X)),
+    retract(itchy_feet(X)),
+    retract(dryness(X)),
+    retract(scaling(X)),
+    retract(contaminated_food_contact(X)),
+    retract(contaminated_water_contact(X)),
+    retract(stomach_pain(X)),
+    retract(hepa_b_infected_contact(X)),
+    retract(light_colored_poop(X)),
+    retract(belly_pain(X)),
+    retract(mosquito_bite(X)).
+
+
+
+
+
+
+
+
+
+
+
